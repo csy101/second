cf250765f239f68e4d1c95d3fb883d6657f98110
csy <csy@.com>
2019-02-22 11:54:12 +0800

    vv1

---
+++ src/test/java/com/baizhi/AppTest.java
@@ -0,0 +1,25 @@
+package com.baizhi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test() {
+        assertTrue(true);
+        System.out.println("22222");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("33333");
+    }
+
+}
